d2998811a898cc82ec1e3db02100f1efa6a3d5c4
Giorgi <mrmrela5@gmail.com>
2023-12-13 19:32:02 +0400

    using the Http hook to GET users

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,35 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import ErrorModal from '../../shared/components/UIElement/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElement/LoadingSpinner';
+import useHttpClient from '../../shared/hooks/http-hook';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
   const [users, setUsers] = useState([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+
+  const { sendRequest, isLoading, error, clearError } = useHttpClient();
 
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const getUsers = async () => {
       try {
-        const res = await fetch('http://localhost:5000/api/users');
+        const res = await sendRequest('http://localhost:5000/api/users');
 
-        const data = await res.json();
-        if (!res.ok) throw new Error(data.message);
-        setUsers(data.users);
-      } catch (err) {
-        setError(err.message);
-      }
-      setIsLoading(false);
+        setUsers(res.users);
+      } catch (err) {}
     };
-    sendRequest();
+    getUsers();
   }, []);
-
-  const errorHandler = () => setError(null);
 
   return (
     <>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
