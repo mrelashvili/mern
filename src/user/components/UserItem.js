7732b607565594118e6216d96735b68d9dc40acd
Giorgi <mrmrela5@gmail.com>
2023-12-27 23:17:43 +0400

    change user image url

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -12,7 +12,7 @@ const UserItem = ({ user }) => {
       <Card className="user-item__content">
         <Link to={`/${_id}/places`}>
           <div className="user-item__image">
-            <Avatar image={image} alt={name} />
+            <Avatar image={`http://localhost:5000/${image}`} alt={name} />
           </div>
           <div className="user-item__info">
             <h2>{name}</h2>
