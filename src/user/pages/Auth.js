7197fdb9aabcbc324acfb8ee0302a33f83d294b9
Giorgi <mrmrela5@gmail.com>
2023-12-27 22:50:38 +0400

    change data format for accepting image also (change from json to formData)

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -68,8 +68,6 @@ const Auth = () => {
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    console.log(formState.inputs);
-
     if (isLogin) {
       try {
         const responseData = await sendRequest(
@@ -85,18 +83,20 @@ const Auth = () => {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append('email', formState.inputs.email.value);
+        formData.append('name', formState.inputs.name.value);
+        formData.append('password', formState.inputs.password.value);
+        formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
-
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            'Content-Type': 'application/json',
-          }
+          formData
+          // JSON.stringify({
+          //   name: formState.inputs.name.value,
+          //   email: formState.inputs.email.value,
+          //   password: formState.inputs.password.value,
+          // }),
         );
         auth.login(responseData.user._id);
       } catch (err) {}
